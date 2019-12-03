cf58974adbe212045c826ff29f4ec4d59701864f
spxy <rok.skrbec93@gmail.com>
2019-12-03 22:25:40 +0100

    first commit

--- js/main.js
+++ js/main.js
@@ -1,13 +1,11 @@
 window.addEventListener("load", () => {
-  var nav = document.querySelector(".main-nav"); // Identify target
-  console.log(nav.he);
+  const nav = document.querySelector(".main-nav"); // Identify target
   window.addEventListener("scroll", function(event) {
     // To listen for event
     event.preventDefault();
-
     if (window.scrollY >= this.window.innerHeight - nav.clientHeight) {
       // Just an example
-      nav.style.backgroundColor = "rgba(0, 0, 0, 0.7)"; // or default color
+      nav.style.backgroundColor = "rgba(1, 1, 1, 1)"; // or default color
     } else {
       nav.style.backgroundColor = "transparent";
     }
