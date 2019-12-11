f657c5918783be7c91045d3ab03d83f50a98afee
spxy <rok.skrbec93@gmail.com>
2019-12-11 21:57:48 +0100

    first commit

--- js/main.js
+++ js/main.js
@@ -30,4 +30,30 @@ window.addEventListener("load", () => {
       mobileMenu.innerHTML = "meni";
     }
   });
+
+  const startPageRotatingText = ["Z", "gasilskim", "pozdravom", "na", "pomoč!"];
+  const startPageRotatingTextLength = startPageRotatingText.length;
+  let startPageRotatingTextIndex = 0;
+  setInterval(() => {
+    document.querySelector(".rotating-text").innerHTML =
+      startPageRotatingText[startPageRotatingTextIndex];
+    startPageRotatingTextIndex =
+      (startPageRotatingTextIndex + 1) % startPageRotatingTextLength;
+  }, 700);
+
+  const startPageRotatingImages = [
+    "./images/start-page-image-01.svg",
+    "./images/start-page-image-02.jpg",
+    "./images/start-page-image-03.jpg",
+    "./images/start-page-image-04.jpg"
+  ];
+  const startPageRotatingImagesLength = startPageRotatingImages.length;
+  let startPageRotatingImagesIndex = 0;
+  setInterval(() => {
+    document.querySelector(".start-page-image img").src =
+      startPageRotatingImages[startPageRotatingImagesIndex];
+    startPageRotatingImagesIndex =
+      (startPageRotatingImagesIndex + 1) % startPageRotatingImagesLength;
+    console.log(startPageRotatingImages[startPageRotatingImagesIndex].src);
+  }, 700);
 });
